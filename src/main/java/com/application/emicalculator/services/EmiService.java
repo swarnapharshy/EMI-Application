9349170f8860972234981d6bc221183cea7e2651
swarnapharshy <swarnapharshy@gmail.com>
2019-07-25 10:22:29 +0530

    Initial Commit

---
+++ src/main/java/com/application/emicalculator/services/EmiService.java
@@ -0,0 +1,7 @@
+package com.application.emicalculator.services;
+
+import com.application.emicalculator.entities.EmiEntity;
+
+public interface EmiService {
+    public double emicalculator(EmiEntity emiEntity);
+}
